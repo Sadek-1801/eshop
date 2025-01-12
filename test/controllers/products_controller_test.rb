a8c62984ee09255980defa3ef2e7cb1ccfd1f62e
Sadek-1801 <hmashrafujjaman@gmail.com>
2025-01-13 00:04:00 +0600

    Add store controller

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -46,4 +46,11 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to products_url
   end
+
+  test "can't delete product in cart" do
+    assert_difference("Product.count", 0) do
+      delete product_url(products(:apple))
+    end
+    assert_redirected_to products_url
+  end
 end
